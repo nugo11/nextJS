3f6598f183c0c8f66831afa7e5f5ed99c0223f00
nugo11 <nugoxucishvili@gmail.com>
2024-08-15 15:46:30 +0400

    sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,8 @@
+// next-sitemap.config.js
+
 module.exports = {
-    siteUrl: 'https://filmebi.in',
-    generateRobotsTxt: true,
+    siteUrl: 'https://filmebi.in', // Replace with your site URL
+    generateRobotsTxt: true, // (Optional) Generate a robots.txt file
+    // Additional options
   };
   
--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,8 @@
 /** @type {import('next').NextConfig} */
-import { defineConfig } from 'next';
-import { withSitemap } from 'next-sitemap';
-
 const nextConfig = {
-  images: {
-    domains: ['filmebi.in'],
-  },
-  // Add any other Next.js configurations here
-};
-
-// Export the configuration with sitemap options
-export default withSitemap(
-  defineConfig({
-    ...nextConfig,
-    // Sitemap options
-    siteUrl: 'https://filmebi.in', // Replace with your site URL
-    generateRobotsTxt: true, // (Optional) Generate a robots.txt file
-  })
-);
+    images: {
+      domains: ['filmebi.in'],
+    },
+  };
+  
+  export default nextConfig;
